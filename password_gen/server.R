2a1c62ea8016d0010b0fba00c3797ada0f8c340a
tcole333 <travis.cole44@gmail.com>
2019-04-03 11:50:27 -0500

    rewrite without dplyr so I can run on ubuntu

--- password_gen/server.R
+++ password_gen/server.R
@@ -1,9 +1,8 @@
 
-library(dplyr)
 library(shiny)
 
 #data 
-eff_list <- read.table("eff_large_wordlist.txt")
+eff_list <- read.table("~/Downloads/eff_large_wordlist.txt")
 
 
 # Define server logic required to draw a histogram
@@ -20,15 +19,6 @@ shinyServer(function(input, output) {
 
 #password gen function
 password_gen <- function(words) {
-  password <- NULL
-  word_len <- words
-  while (word_len >= 1) {
-    x <- floor(runif(5, 1, 7))
-    first_num <- paste(c(toString(x[1]),toString(x[2]), toString(x[3]),toString(x[4]), toString(x[5])), sep = "", collapse = "")
-    pass <- filter(eff_list, eff_list$V1 == first_num)
-    w <- pass$V2
-    password <- c(password, toString(w))
-    word_len = word_len - 1
-  }
-  paste(password, collapse = "")
+  password <- paste(sample(eff_list$V2, words), sep = '', collapse = '')
+  return(password)
 }
